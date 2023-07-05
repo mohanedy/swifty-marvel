e78ce35e4a230a7fc7f2d7430a70782dcb285141
Mohaned Yossry <m.yossry@walaplus.com>
2023-07-05 12:40:14 +0300

    Initial Commit

---
+++ SwiftyMarvel/SwiftyMarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyMarvelApp.swift
+//  SwiftyMarvel
+//
+//  Created by Mohaned Yossry on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyMarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
