0a7eeabe15d0d50b8daad418ed3d36372743829f
Mohaned Yossry <memo.yossry@gmail.com>
2023-07-08 00:45:21 +0300

    👷 build(Data): Add base api url

---
+++ SwiftyMarvel/Data/Constants/APIConstants.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstants.swift
+//  SwiftyMarvel
+//
+//  Created by Mohaned Yossry on 07/07/2023.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let baseURL = "https://gateway.marvel.com/v1/public/"
+}
