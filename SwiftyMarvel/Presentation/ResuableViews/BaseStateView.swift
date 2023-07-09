e8cdbfb149870708c1a218bbc9cbc338b2cdc9ef
Mohaned Yossry <memo.yossry@gmail.com>
2023-07-10 00:54:15 +0300

    📖 docs: Add dev docs

--- SwiftyMarvel/Presentation/ResuableViews/BaseStateView.swift
+++ SwiftyMarvel/Presentation/ResuableViews/BaseStateView.swift
@@ -7,13 +7,24 @@
 
 import SwiftUI
 
+/// Handle changes in the state of the given [ViewModel] and display the appropriate view.
 struct BaseStateView: View {
     @ObservedObject var viewModel: ViewModel
     let successView: AnyView
     let emptyView: AnyView?
     let errorView: AnyView?
     let loadingView: AnyView?
-
+    
+    /// Initialize the view with the given [ViewModel] and views to display in each state.
+    ///
+    /// - Parameters:
+    ///  - viewModel: The [ViewModel] to observe its state.
+    ///  - successView: The view to display when the state is [ViewState.success].
+    ///  - emptyView: The view to display when the state is [ViewState.empty].
+    ///  - errorView: The view to display when the state is [ViewState.error].
+    ///  - loadingView: The view to display when the state is [ViewState.loading].
+    ///
+    ///  - Note: The default value for each view is nil, so you have to provide at least the successView.
     init(viewModel: ViewModel,
          successView: AnyView,
          emptyView: AnyView? = nil,
@@ -25,7 +36,7 @@ struct BaseStateView: View {
         self.errorView = errorView
         self.loadingView = loadingView
     }
-    
+
     var body: some View {
         ZStack {
             successView
