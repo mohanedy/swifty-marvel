60c52829c8a72dc45cd203cb743a1935d4857140
Mohaned Yossry <memo.yossry@gmail.com>
2023-07-12 14:33:26 +0300

    🚨 test: Add MockingBird framework to mock dependencies instead of manual mocking

--- SwiftyMarvelTests/Presentaion/Screens/Home/ViewModels/HomeViewModelTests.swift
+++ SwiftyMarvelTests/Presentaion/Screens/Home/ViewModels/HomeViewModelTests.swift
@@ -7,89 +7,91 @@
 
 import Foundation
 import XCTest
+import Mockingbird
 @testable import SwiftyMarvel
 
 // swiftlint:disable force_unwrapping
+@MainActor
 final class HomeViewModelTests: XCTestCase {
     // MARK: - Properties
     var sut: HomeViewModel!
-    var mockGetCharactersUseCase: MockGetCharactersUseCase!
-
+    var getCharactersUCMock: GetCharactersUCMock!
+    
     // MARK: - Fake Data
     let fakeCharacter1 = Character(id: 1, name: "Spider-Man")
     let fakeCharacter2 = Character(id: 2, name: "Iron Man")
     let fakeCharacter3 = Character(id: 3, name: "Hulk")
     let fakeCharacter4 = Character(id: 4, name: "Thor")
-
+    
     // MARK: - Setup and Teardown
-    @MainActor
     override func setUp() {
         super.setUp()
-        mockGetCharactersUseCase = MockGetCharactersUseCase()
-        sut = HomeViewModel(getCharactersUseCase: mockGetCharactersUseCase, debounceTime: 0)
+        getCharactersUCMock = mock(GetCharactersUC.self)
+        sut = HomeViewModel(getCharactersUseCase: getCharactersUCMock, debounceTime: 0)
     }
-
+    
     override func tearDown() {
         sut = nil
-        mockGetCharactersUseCase = nil
+        getCharactersUCMock = nil
         super.tearDown()
     }
-
+    
     // MARK: - Tests
-    @MainActor
     func testLoadCharactersSuccess() async throws {
         // Given
         let expectedCharacters = [fakeCharacter1, fakeCharacter2]
-        let expectedTotalCount = 2
-        mockGetCharactersUseCase.result = .success(PaginatedResponse(offset: 0,
-                                                                     limit: 2,
-                                                                     total: 2,
-                                                                     count: 2,
-                                                                     results: expectedCharacters))
+        await given(getCharactersUCMock.execute(with: any())).willReturn(.success(
+            PaginatedResponse(offset: 0,
+                              limit: 2,
+                              total: 2,
+                              count: 2,
+                              results: expectedCharacters)))
+        
         // When
         await sut.loadCharacters()
+        
         // Then
         XCTAssertEqual(sut.characters, expectedCharacters)
-        XCTAssertEqual(sut.totalCount, expectedTotalCount)
         XCTAssertEqual(sut.state, .success)
     }
-
-    @MainActor
+    
     func testLoadCharactersFailure() async throws {
         // Given
         let expectedError = AppError.networkError("Network Error")
-        mockGetCharactersUseCase.result = .failure(expectedError)
+        await given(getCharactersUCMock.execute(with: any()))
+            .willReturn(.failure(expectedError))
+        
         // When
         await sut.loadCharacters()
+        
         // Then
         XCTAssertTrue(sut.characters.isEmpty)
         XCTAssertEqual(sut.state, .error(expectedError.localizedDescription))
     }
-
-    @MainActor
+    
     func testSearchCharactersSuccess() async throws {
         // Given
         let expectedSearchText = "Hulk"
         let expectedDebouncedSearchText = "Hulk"
         let expectedCharacters = [fakeCharacter3]
-        let expectedTotalCount = 1
-        mockGetCharactersUseCase.result = .success(PaginatedResponse(offset: 0,
-                                                                     limit: 1,
-                                                                     total: 1,
-                                                                     count: 1,
-                                                                     results: expectedCharacters))
+        await  given(getCharactersUCMock.execute(with: any())).willReturn(.success(
+            PaginatedResponse(offset: 0,
+                              limit: 1,
+                              total: 1,
+                              count: 1,
+                              results: expectedCharacters)))
+        
         // When
         sut.searchText = expectedSearchText
         await sut.searchCharacters()
+        
         // Then
         XCTAssertEqual(sut.searchText, expectedSearchText)
         XCTAssertEqual(sut.debouncedSearchText, expectedDebouncedSearchText)
         XCTAssertEqual(sut.characters, expectedCharacters)
-        XCTAssertEqual(sut.totalCount, expectedTotalCount)
         XCTAssertEqual(sut.state, .success)
     }
-
-    @MainActor
+    
     func testLoadMoreCharactersIfNeededSuccess() async throws {
         // Given
         let initialCharacters = [fakeCharacter1,
@@ -98,35 +100,34 @@ final class HomeViewModelTests: XCTestCase {
                               fakeCharacter4]
         let allCharacters = initialCharacters + moreCharacters
         let initialTotalCount = 4
-        mockGetCharactersUseCase.result = .success(PaginatedResponse(offset: 0,
-                                                                     limit: 2,
-                                                                     total: initialTotalCount,
-                                                                     count: 2,
-                                                                     results: initialCharacters))
+        await given(getCharactersUCMock.execute(with: any())).willReturn(.success(
+            PaginatedResponse(offset: 0,
+                              limit: 2,
+                              total: initialTotalCount,
+                              count: 2,
+                              results: initialCharacters)))
+        
         // When
         await sut.loadCharacters()
+        
         // Then
         XCTAssertEqual(sut.characters.count, initialCharacters.count)
+        
         // Given
-         mockGetCharactersUseCase.result = .success(PaginatedResponse(offset: 2,
-                                                                      limit: 2,
-                                                                      total: initialTotalCount,
-                                                                      count: 2,
-                                                                      results: moreCharacters))
-         // When
-         await sut.loadMoreCharactersIfNeeded(currentItem: sut.characters.last!)
-         // Then
-         XCTAssertEqual(sut.characters.count, allCharacters.count)
-         XCTAssertEqual(sut.characters.last?.id, allCharacters.last?.id)
-         XCTAssertEqual(sut.state, .success)
-    }
-}
-
-// MARK: - Mocks
-class MockGetCharactersUseCase: GetCharactersUC {
-    var result: Result<PaginatedResponse<Character>, AppError>!
-    func execute(with params: GetCharactersParams) async -> Result<PaginatedResponse<Character>, AppError> {
-       return result
+        await given(getCharactersUCMock.execute(with: any())).willReturn(.success(
+            PaginatedResponse(offset: 2,
+                              limit: 2,
+                              total: initialTotalCount,
+                              count: 2,
+                              results: moreCharacters)))
+        
+        // When
+        await sut.loadMoreCharactersIfNeeded(currentItem: sut.characters.last!)
+        
+        // Then
+        XCTAssertEqual(sut.characters.count, allCharacters.count)
+        XCTAssertEqual(sut.characters.last?.id, allCharacters.last?.id)
+        XCTAssertEqual(sut.state, .success)
     }
 }
 // swiftlint:enable force_unwrapping
