5d989f6a9b7175296346ce7071608ee5dab0680e
Mohaned Yossry <memo.yossry@gmail.com>
2023-07-17 22:25:05 +0300

    ♻️ refactor: Refactor CachedImageView to use Nuke instead of KF

--- SwiftyMarvel/Presentation/ResuableViews/CachedImageView.swift
+++ SwiftyMarvel/Presentation/ResuableViews/CachedImageView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import Kingfisher
+import NukeUI
 
 struct CachedImageView: View {
     
@@ -17,17 +17,34 @@ struct CachedImageView: View {
     }
     
     var body: some View {
-        KFImage.url(url)
-            .placeholder({
-                ProgressView()
-            })
-            .fade(duration: 0.25)
+        LazyImage(url: url) { state in
+            if let image = state.image {
+                image.resizable()
+            } else if state.error != nil {
+                errorView
+            } else {
+                loadingView
+            }
+            
+        }
+    }
+    
+    private var loadingView: some View {
+        ZStack(alignment: .center) {
+            Color.gray.aspectRatio(contentMode: .fill)
+            ProgressView()
+        }//:- ZStack
+    }
+    
+    private var errorView: some View {
+        Image(systemName: "photo")
             .resizable()
+            .aspectRatio(contentMode: .fit)
     }
 }
 
 struct CachedImageView_Previews: PreviewProvider {
     static var previews: some View {
-        CachedImageView(nil)
+        CachedImageView(URL(string: "https://example.com/image.jpeg"))
     }
 }
--- SwiftyMarvel/Presentation/Screens/Home/Views/CharacterView.swift
+++ SwiftyMarvel/Presentation/Screens/Home/Views/CharacterView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import Kingfisher
 
 struct CharacterView: View {
     
