76d41f9b7ab0916b48499c8999e2048633871328
Mohaned Yossry <memo.yossry@gmail.com>
2023-09-02 21:24:43 +0300

    ♻️ refactor: Improve HomeView structure

--- SwiftyMarvel/Presentation/Screens/Home/Views/HomeView.swift
+++ SwiftyMarvel/Presentation/Screens/Home/Views/HomeView.swift
@@ -9,26 +9,15 @@ import SwiftUI
 
 struct HomeView: View {
     @StateObject private var viewModel = Resolver.shared.resolve(HomeViewModel.self)
+    
     var body: some View {
         NavigationStack {
             ZStack {
                 BaseStateView(
                     viewModel: viewModel,
                     successView: AnyView(
-                        ScrollView {
-                            LazyVStack {
-                                ForEach(viewModel.characters) { item in
-                                    NavigationLink(
-                                        destination: CharacterProfileView(character: item)) {
-                                            CharacterView(character: item)
-                                                .task {
-                                                    await viewModel.loadMoreCharactersIfNeeded(currentItem: item)
-                                                }
-                                        }
-                                }
-                            }
-                        }
-                            .padding()
+                        content
+                            .padding([.leading, .trailing], 15)
                             .navigationTitle("swiftyMarvel".localized())
                             .searchable(text: $viewModel.searchText,
                                         prompt: "typeCharacterName".localized())
@@ -40,15 +29,33 @@ struct HomeView: View {
                     )//: AnyView
                 )//: BaseStateView
             } //: ZStack
+            .task {
+                await viewModel.loadCharacters()
+            }
         } //: NavigationStack
-        .task {
-            await viewModel.loadCharacters()
+    }
+    
+    var content: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(viewModel.characters) { item in
+                    NavigationLink(
+                        destination: CharacterProfileView(character: item)
+                            .toolbar(.hidden, for: .tabBar)
+                    ) {
+                        CharacterView(character: item)
+                            .task {
+                                await viewModel
+                                    .loadMoreCharactersIfNeeded(currentItem: item)
+                            }
+                    }
+                }
+            }
         }
+        .scrollIndicators(.hidden)
     }
 }
 
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+#Preview {
+    HomeView()
 }
