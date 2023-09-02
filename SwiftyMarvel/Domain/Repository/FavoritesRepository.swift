4c9e20f4dfa2d96a92569743cc1c0fc7e042bf08
Mohaned Yossry <memo.yossry@gmail.com>
2023-09-02 21:40:12 +0300

    📖 docs: Add dev docs

--- SwiftyMarvel/Domain/Repository/FavoritesRepository.swift
+++ SwiftyMarvel/Domain/Repository/FavoritesRepository.swift
@@ -8,8 +8,16 @@
 import Foundation
 
 protocol FavoritesRepository {
+    
+    /// Get all favorites characters from local storage
     func getFavorites() -> Result<[Character], AppError>
+    
+    /// Check if character is favorite or not
     func isFavorite(character: Character) -> Bool
+    
+    /// Add character to favorites
     func addFavorite(character: Character)
+    
+    /// Remove character from favorites
     func removeFavorite(character: Character)
 }
