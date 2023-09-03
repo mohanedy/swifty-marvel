76ab54a47a5c264f189d8675de92abb200e9d3d6
Mohaned Yossry <memo.yossry@gmail.com>
2023-09-03 21:12:27 +0300

    🚨 test(FavoritesDataSourceTests): Add unit tests

---
+++ SwiftyMarvelTests/Data/DataSource/Local/TestCoreDataStack.swift
@@ -0,0 +1,30 @@
+//
+//  TestCoreDataStack.swift
+//  SwiftyMarvelTests
+//
+//  Created by Mohaned Yossry on 03/09/2023.
+//
+
+import XCTest
+import CoreData
+import SwiftyMarvel
+
+class TestCoreDataStack: CoreDataStack {
+    override init() {
+        super.init()
+        
+        let container = NSPersistentContainer(name: CoreDataStack.modelName,
+                                              managedObjectModel: CoreDataStack.model)
+        let description = NSPersistentStoreDescription()
+        description.type = NSInMemoryStoreType
+        container.persistentStoreDescriptions = [description]
+        
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        
+        self.storeContainer = container
+    }
+}
