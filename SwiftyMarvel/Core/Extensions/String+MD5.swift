2e0cd9bdcaddd3af1f385f609070b21ea345e0b6
Mohaned.Y <mohaned.y@icloud.com>
2025-09-19 20:04:56 +0300

    ♻️ refactor: Migrate to `CryptoKit`

--- SwiftyMarvel/Core/Extensions/String+MD5.swift
+++ SwiftyMarvel/Core/Extensions/String+MD5.swift
@@ -6,20 +6,12 @@
 //
 
 import Foundation
-import CommonCrypto
+import CryptoKit
 
 extension String {
     var md5: String {
-        let length = Int(CC_MD5_DIGEST_LENGTH)
-        var digest = [UInt8](repeating: 0, count: length)
-        if let data = data(using: String.Encoding.utf8) {
-            _ = data.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
-                CC_MD5(body, CC_LONG(data.count), &digest)
-            }
-        }
-
-        return (0..<length).reduce("") {
-            $0 + String(format: "%02x", digest[$1])
-        }
+        let data = Data(self.utf8)
+        let digest = Insecure.MD5.hash(data: data)
+        return digest.map { String(format: "%02x", $0) }.joined()
     }
 }
